e7a2388c2b9b650505860717d7a9d1645981fd35
shaleila <shaleila.louis@gmail.com>
2019-05-20 10:54:32 -0400

    new reqd intents added

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const LaunchRequestHandler = {
 const PauseIntentHandler = {
  	canHandle(handlerInput) {
     	return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      	&& handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PauseIntent';
+      	&& (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.PauseIntent'
+      	|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent');
   	},
 		handle(handlerInput) {
    		return handlerInput.responseBuilder
